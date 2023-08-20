bcd1aef3b3e1215786f0ac5799696c4f93e6eca9
agus-darmawan <5024211070@mhs.its.ac.id>
2023-08-20 17:48:45 +0700

    feat: add vision and mision component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
   return (
     <MainLayout>
       <Seo templateTitle='Home' />
-      <div className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-white to-[D0D5EB]'>
+      <div className='flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-white to-[#D0D5EB]'>
         <h1 className='text-blue-starter text-3xl font-bold drop-shadow-2xl md:text-5xl lg:text-6xl '>
           #SemuaBisaBelajar
         </h1>
@@ -28,6 +28,53 @@ export default function HomePage() {
           </div>
         </div>
       </div>
+      <div className=' min-h-screen bg-[#D0D5EB]'></div>
+      <div className='bg-blue-starter min-h-screen'></div>
+      <div className='bg-blue-backgraund flex min-h-screen flex-col items-center justify-center'>
+        <h2 className='text-blue-starter pb-2 text-xl md:text-2xl lg:text-3xl '>
+          #SemuaBisaBelajar
+        </h2>
+        <h1 className='text-blue-starter text-3xl font-bold drop-shadow-2xl md:text-5xl lg:text-6xl '>
+          Starter Academy
+        </h1>
+        <p className='text-blue-starter max-w-3xl px-10 pt-2  text-justify text-lg md:text-center md:text-xl lg:text-center'>
+          Starter Academy merupakan platform pembelajaran yang relevan dan
+          bekualitas tinggi serta mudah di akses oleh siapapun. Starter Academy
+          hadir untuk mencipatakan pendidikan yang mendukung potensi setiap
+          individu agar memiliki pendidikan yang berkualitas.
+        </p>
+        <div className=' pt-28 md:block '>
+          <div className='border-blue-starter animate-bounce rounded-full border-2'>
+            <IconChevronDown className='mx-auto h-12 w-12  p-2 text-8xl font-bold' />
+          </div>
+        </div>
+      </div>
+      <div className='from-blue-backgraund bg-gradient-to-b to-white pb-20 lg:pb-32 '>
+        <div className='container mx-auto flex grid-cols-2 flex-col gap-12 md:grid md:gap-16 lg:gap-28'>
+          <div className='cold-end-2 col-start-1'>
+            <h1 className='text-blue-starter text-center text-3xl md:text-4xl lg:text-5xl'>
+              Visi
+            </h1>
+            <p className='text-blue-starter max-w-3xl px-10 pt-2  text-center text-lg md:text-center md:text-xl'>
+              Menyediakan materi pembelajaran yang relevan dan berkualitas
+              tinggi, serta mendukung pengembangan diri secara holistik melalui
+              pendekatan yang inklusif dan berorientasi pada kebutuhan
+              mahasiswa.
+            </p>
+          </div>
+          <div className='cold-end-3 col-start-2 '>
+            <h1 className='text-blue-starter text-center text-3xl md:text-4xl lg:text-5xl'>
+              Misi
+            </h1>
+            <p className='text-blue-starter max-w-3xl px-10 pt-2  text-center text-lg md:text-center md:text-xl'>
+              Untuk menciptakan dunia di mana pendidikan tidak terikat oleh
+              batasan-batasan, dan setiap individu memiliki kesempatan untuk
+              menggali potensi penuh mereka melalui pengetahuan akademik.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className='bg-blue-starter min-h-screen'></div>
     </MainLayout>
   );
 }
